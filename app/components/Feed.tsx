d752ddca4ebde1e9f1261eb2ba2132a3ab3c86f6
girum <girumkenenisa@gmail.com>
2025-08-16 23:54:49 +0300

    make the feed section responsive

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -4,15 +4,15 @@ import Posts from './Posts'
 
 const Feed = () => {
   return (
-   <main>
-    <section>
+   <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+    <section className='md:col-span-2'>
       <Stories/>
       {/* stories */}
       {/* posts */}
       <Posts/>
     </section>
 
-    <section>
+    <section className='hidden md:inline-grid '>
       {/* mini-profile */}
       {/* suggestions */}
     </section>
