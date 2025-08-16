8d2cf1319ea4a1d3761c1c3856df7c55150eed71
girum <girumkenenisa@gmail.com>
2025-08-16 21:35:28 +0300

    style story section and intall tailwind scrollbar

--- app/components/Story.tsx
+++ app/components/Story.tsx
@@ -7,8 +7,8 @@ type storyProps = {
 const Story :React.FC<storyProps>= ({avatar,name}) => {
   return (
     <div>
-        <img src={avatar} alt={name} />
-        <p>{name}</p>
+        <img className='h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out' src={avatar} alt={name} />
+        <p className='text-xs w-14 truncate'>{name}</p>
     </div>
   )
 }
